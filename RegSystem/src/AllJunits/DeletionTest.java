e58d602b0e6a490c8dcd7477ed88c208cb909fb5
das17 <das135mist@gmail.com>
2017-10-28 06:53:36 +0600

    Testing a particular object is present in the list of object

--- RegSystem/src/AllJunits/DeletionTest.java
+++ RegSystem/src/AllJunits/DeletionTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import AllActions.AppliedStudent;
@@ -20,8 +21,11 @@ public class DeletionTest {
 	 
 	 assertTrue(list.isEmpty());
 	 
-	 //AppliedStudent student=new AppliedStudent("", lastName, id, gender);
+	 AppliedStudent student=new AppliedStudent("John", "Smith", 1, "male");
 	 
+	 assertThat(list, CoreMatchers.hasItem(student));;
+	 
+	 AppliedStudent student2=new AppliedStudent("Laura", "Smith", 2, "female");
 	 
 	}
 
